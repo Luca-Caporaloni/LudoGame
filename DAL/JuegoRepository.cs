550aeb5708a706ff735046f86515b89bc4d31bfb
PinkyGames <lucacaporaloni@gmail.com>
2024-10-24 18:00:59 -0300

    Update JuegoRepository.cs

--- DAL/JuegoRepository.cs
+++ DAL/JuegoRepository.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class JuegoRepository
+    public class JuegoRepository
     {
+        // Simulación de guardado de partida (más adelante se conectará a una base de datos)
+        public void GuardarPartida(List<string> jugadores)
+        {
+            // Lógica para guardar los datos de los jugadores en una base de datos
+        }
+
+        public List<string> CargarPartida()
+        {
+            // Lógica para cargar datos de los jugadores de una base de datos
+            return new List<string> { "Jugador 1", "Jugador 2" };
+        }
     }
 }
